68c7b3219f5995901d8e98c174cc3aac51c24bcf
sdelappe <sdelappe57@gmail.com>
2021-10-30 10:31:32 -0700

    finished exercise

---
+++ API_Exercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace API_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            //Kanye Quote
+            var kanyeURL = "https://api.kanye.rest";
+
+            //Swanson Quote
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            for (int i = 0; i < 5; i++)
+            {
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            
+                Console.WriteLine("Kanye: " + kanyeQuote);
+                Console.WriteLine();
+                Console.WriteLine("Ron: " + ronQuote);
+                Console.WriteLine();
+                Console.WriteLine("===================");
+                Console.WriteLine();
+            }
+        }
+    }
+}
